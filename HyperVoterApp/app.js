1c2df55ca1986eb323a163479354eab37ec3b124
harpreetvirkk <harpreetvirk2018@gmail.com>
2020-04-28 21:21:52 +0530

    app.js

--- HyperVoterApp/app.js
+++ HyperVoterApp/app.js
@@ -6,12 +6,12 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 
-// const {FileSystemWallet, Gateway, X509WalletMixin} = require('fabric-network');
+const {FileSystemWallet, Gateway, X509WalletMixin} = require('fabric-network');
 const fs = require('fs');
 
-// const ccpPath = path.resolve(__dirname, '..', 'basic-network', 'connection.json');
-// const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
-// const ccp = JSON.parse(ccpJSON);
+const ccpPath = path.resolve(__dirname, '..', 'basic-network', 'connection.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
 
 var app = express();
 app.use(session({
@@ -21,6 +21,38 @@ app.use(session({
 }));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
+
+//Enroll Admin if Not already installed
+async function main() {
+    try{
+        // Check to see if we've already enrolled the admin user.
+        const adminExists = await wallet.exists('admin');
+        if (adminExists) {
+            console.log('An identity for the admin user "admin" already exists in the wallet');
+            console.log('Please continue');
+            return;
+        } else {
+            // Create a new CA client for interacting with the CA.
+            const caURL = ccp.certificateAuthorities['ca.example.com'].url;
+            const ca = new FabricCAServices(caURL);
+
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = new FileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+
+            // Enroll the admin user, and import the new identity into the wallet.
+            const enrollment = await ca.enroll({ enrollmentID: 'admin', enrollmentSecret: 'adminpw' });
+            const identity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
+            wallet.import('admin', identity);
+            console.log('Successfully enrolled admin user "admin" and imported it into the wallet');
+        }
+
+    } catch (error){
+        console.log(`Failed to enroll admin user "admin": ${error}`);
+        console.log('Please run enrollAdmin.js in a seperate terminal window before continuing!\n');
+    }
+};
 
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/public/index.html'));
@@ -47,16 +79,16 @@ app.post('/EC-dashboard/EC-lgin', function(request, response) {
                 request.session.loggedin = true;
                 request.session.username = username;
             } else {
-                response.send('Incorrect EC username!');
-                response.end();
+                console.log('Incorrect EC username!');
+                response.redirect('/EC-dashboard/ec-login.html');
             }
         } else {
-            response.send('Incorrect EC password!');
-            response.end();
+            console.log('Incorrect EC password!');
+            response.redirect('/EC-dashboard/ec-login.html');
         }
     } else {
-        response.send('Please enter Username and Password!');
-        response.end();
+        console.log('Please enter Username and Password!');
+        response.redirect('/EC-dashboard/ec-login.html');
     }
     console.log("Credentials verified, loggin the EC in!");
     response.redirect('/EC-dashboard/ec-dashboard.html');
@@ -72,13 +104,16 @@ app.post('/EC-dashboard/EC-reg', function(request, response) {
                 request.session.loggedin = true;
                 request.session.username = username;
             } else {
-                response.send('Incorrect EC username!');
+                console.log('Incorrect EC username!');
+                response.redirect('/EC-dashboard/ec-registration.html');
             }
         } else {
-            response.send('Incorrect EC password!');
+            console.log('Incorrect EC password!');
+            response.redirect('/EC-dashboard/ec-registration.html');
         }
     } else {
-        response.send('Please enter Username and Password!');
+        console.log('Please enter Username and Password!');
+        response.redirect('/EC-dashboard/ec-registration.html');
     }
     console.log("Credentials verified, Registering EC account!");
 
@@ -95,6 +130,7 @@ app.post('/EC-dashboard/EC-reg', function(request, response) {
                 const EC_Exists = await wallet.exists(EC_ID);
                 if (EC_Exists) {
                     console.log(`An identity for the EC already exists in the wallet`);
+                    response.redirect('/EC-dashboard/index.html');
                     return;
                 }
 
@@ -103,6 +139,7 @@ app.post('/EC-dashboard/EC-reg', function(request, response) {
                 if (!adminExists) {
                     console.log('An identity for the admin user "admin" does not exist in the wallet');
                     console.log('Run the enrollAdmin.js application before retrying');
+                    response.redirect('/');
                     return;
                 }
 
@@ -127,9 +164,10 @@ app.post('/EC-dashboard/EC-reg', function(request, response) {
                 console.log(`Successfully registered and enrolled EC with enrollment ID 1 and imported it into the wallet`);
 
             } catch (error) {
-                console.error(`Failed to register EC: ${error}`);
-                response.end();
-                process.exit(1);    
+                console.log(`Failed to register EC: ${error}`);
+                response.redirect('/');
+                // response.end();
+                // process.exit(1);    
             }
         }
         registerEC();
@@ -145,17 +183,32 @@ app.get('/EC-dashboard/ec-dashboard-add-voter.html', function(request, response)
     response.sendFile(path.join(__dirname + '/public/EC-dashboard/ec-dashboard-add-voter.html'));
 });
 
+var curr_voter_id = 0; //Global current voter id for sequential allocation
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function random_pin(min, max) {  
+    return Math.floor(
+      Math.random() * (max - min) + min
+    )
+}
+
 app.post('/EC-dashboard/EC-addNewVoter', function(request, response) {
-    var voter_id = request.body.newVoterID;
-    var voter_pin = request.body.newVoterPIN;
+    // var voter_id = request.body.newVoterID;
+    // var voter_pin = request.body.newVoterPIN;
     var voter_email = request.body.newVoterEmail;
+    curr_voter_id = curr_voter_id + 1;
+    var voter_id = curr_voter_id;
+    var voter_pin = random_pin(111, 999);
+    var voter_pin_hashed = crypto.createHash('sha256').update(voter_pin).digest('base64');
 
     //  START - Reading Voters List and Writing to it
     let rawdata_voters = fs.readFileSync('voters.json');
     let voters_list = JSON.parse(rawdata_voters);
     var new_voter_json = {
         "voterId" : voter_id,
-        "pin" : voter_pin
+        "pin" : voter_pin_hashed
     };
     voters_list.push(new_voter_json);
     let new_voters_list = JSON.stringify(voters_list);
@@ -203,6 +256,7 @@ app.post('/EC-dashboard/EC-addNewVoter', function(request, response) {
                     const userExists = await wallet.exists(userId);
                     if (!userExists) {
                         console.log(`An identity for the EC ${userId} does not exist in the wallet`);
+                        response.redirect('/EC-dashboard/index.html');
                         return;
                     }
 
@@ -224,8 +278,9 @@ app.post('/EC-dashboard/EC-addNewVoter', function(request, response) {
                     await gateway.disconnect();
 
                 } catch (error) {
-                    console.error(`Failed to submit transaction: ${error}`);
-                    process.exit(1);
+                    console.log(`Failed to submit transaction: ${error}`);
+                    response.redirect('/EC-dashboard/ec-dashboard.html');
+                    // process.exit(1);
                 }
             }
             createVoteObj();
@@ -236,6 +291,11 @@ app.post('/EC-dashboard/EC-addNewVoter', function(request, response) {
 
 app.get('/EC-dashboard/ec-dashboard-add-candidate.html', function(request, response) {
     response.sendFile(path.join(__dirname + '/public/EC-dashboard/ec-dashboard-add-candidate.html'));
+});
+
+app.get('/EC-dashboard/home-page', function(request, response) {
+    request.session.loggedin = false;
+    response.redirect('/'); 
 });
 
 app.post('/EC-dashboard/EC-addNewCandidate', function(request, response) {
@@ -278,12 +338,13 @@ app.post('/voter-dashboard/voter-lgin', function(request, response) {
     let flag = 0;
 
     if (username && password) {
+        var voter_pin_hashed = crypto.createHash('sha256').update(password).digest('base64');
         for (let i = 0; i<voters_list.length; i++){
             if (voters_list[i].voterId == username){
                 flag = 1;
-                if (voters_list[i].pin != password){
-                    response.send("Incorrect PIN");
-                    response.end()
+                if (voters_list[i].pin != voter_pin_hashed){
+                    console.log("Incorrect PIN");
+                    response.redirect('/voter-dashboard/voter-login.html');
                     break;
                 } else {
                     console.log("Identity Verified!\n");
@@ -295,8 +356,8 @@ app.post('/voter-dashboard/voter-lgin', function(request, response) {
         }
 
         if (flag != 1){
-            response.send("You have not been pre-approved by EC to register as a voter!");
-            response.end();
+            console.log("You have not been pre-approved by EC to register as a voter!");
+            response.redirect('/');
         }
         response.redirect('/voter-dashboard/voter-dashboard.html');
         response.end();
@@ -313,12 +374,13 @@ app.post('/voter-dashboard/voter-reg', function(request, response){
     let voters_list = JSON.parse(rawdata_voters);
 
     if (username && password) {
+        var voter_pin_hashed = crypto.createHash('sha256').update(password).digest('base64');
         for (let i = 0; i<voters_list.length; i++){
             if (voters_list[i].voterId == username){
                 flag = 1;
-                if (voters_list[i].pin != password){
-                    response.send("Incorrect PIN");
-                    response.end()
+                if (voters_list[i].pin != voter_pin_hashed){
+                    console.log("Incorrect PIN");
+                    response.redirect('/voter-dashboard/voter-registration.html');
                     break;
                 } else {
                     console.log("Identity Verified!\n");
@@ -330,8 +392,8 @@ app.post('/voter-dashboard/voter-reg', function(request, response){
         }
 
         if (flag != 1){
-            response.send("You have not been pre-approved by EC to register as a voter!");
-            response.end();
+            console.log("You have not been pre-approved by EC to register as a voter!");
+            response.redirect('/voter-dashboard/voter-registration.html');
         }
         console.log("Credentials verified, Registering voter account!");
         if(request.session.loggedin) {
@@ -347,7 +409,7 @@ app.post('/voter-dashboard/voter-reg', function(request, response){
                     const voterExists = await wallet.exists(voter);
                     if (voterExists) {
                         console.log(`An identity for the voter ${voter} already exists in the wallet`);
-                        return;
+                        response.redirect('/voter-dashboard/voter-login.html');
                     }
 
                     // Check to see if we've already enrolled the admin user.
@@ -355,6 +417,7 @@ app.post('/voter-dashboard/voter-reg', function(request, response){
                     if (!adminExists) {
                         console.log('An identity for the admin user "admin" does not exist in the wallet');
                         console.log('Run the enrollAdmin.js application before retrying');
+                        response.redirect('/');
                         return;
                     }
 
@@ -377,9 +440,9 @@ app.post('/voter-dashboard/voter-reg', function(request, response){
                     wallet.import(voter, userIdentity);
                     console.log(`Successfully registered and enrolled voter ${voter} and imported it into the wallet`);
                 } catch (error) {
-                    console.error(`Failed to register voter: ${error}`);
-                    response.end();
-                    process.exit(1); 
+                    console.log(`Failed to register voter: ${error}`);
+                    response.redirect('/voter-dashboard/index.html');
+                    // process.exit(1); 
                 }
             }
         registerVoter();
@@ -410,7 +473,7 @@ app.post('/voter-dashboard/voting', function(request, response){
                     const userExists = await wallet.exists(userId);
                     if (!userExists) {
                         console.log(`An identity for the EC ${userId} does not exist in the wallet`);
-                        return;
+                        response.redirect('/');
                     }
 
                     // Create a new gateway for connecting to our peer node.
@@ -431,8 +494,8 @@ app.post('/voter-dashboard/voting', function(request, response){
                     await gateway.disconnect();
 
                 } catch (error){
-                    console.error(`Failed to submit transaction: ${error}`);
-                    process.exit(1);
+                    console.log(`Failed to submit transaction: ${error}`);
+                    response.redirect('/');
                 }
             }
             sendVoteObj();
@@ -447,76 +510,19 @@ app.post('/voter-dashboard/voting', function(request, response){
 //     response.sendFile(path.join(__dirname + '/public/thankyou_forvoting.html'));
 // });
 
+app.get('/public-bulletin/queryAllVotes.html', function(request, response) {
+    response.sendFile(path.join(__dirname + '/public/public-bulletin/queryAllVotes.html'));
+});
+
+app.get('/public-bulletin/results.html', function(request, response) {
+    response.sendFile(path.join(__dirname + '/public/public-bulletin/results.html'));
+});
+
+main();
+
 var server = app.listen(4000, function () {
   var host = 'localhost'
   var port = server.address().port
   
   console.log("Example app listening at http://%s:%s", host, port)
 })
-
-
-
-
-
-
-
-
-
-// //var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// //var cookieParser = require('cookie-parser');
-// //var logger = require('morgan');
-
-// const {FileSystemWallet, Gateway} = require('fabric-network');
-// const fs = require('fs');
-// //const path = require('path');
-
-// let rawdata_voters = fs.readFileSync('voters.json');
-// let voters_list = JSON.parse(rawdata_voters);
-// const ccpPath = path.resolve(__dirname, '..', '..', 'basic-network', 'connection.json');
-// const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
-// const ccp = JSON.parse(ccpJSON);
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-// app.use(session({
-// 	secret: 'secret',
-// 	resave: true,
-// 	saveUninitialized: true
-// }));
-// app.use(bodyParser.urlencoded({extended : true}));
-// app.use(bodyParser.json());
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
