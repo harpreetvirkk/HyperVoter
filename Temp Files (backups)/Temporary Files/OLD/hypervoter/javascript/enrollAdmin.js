c7a31cdf3c24eb7b921ced893b840ddb82ca9ddb
harpreetvirkk <harpreetvirk2018@gmail.com>
2020-04-30 17:39:30 +0530

    working

--- Temp Files (backups)/Temporary Files/OLD/hypervoter/javascript/enrollAdmin.js
+++ Temp Files (backups)/Temporary Files/OLD/hypervoter/javascript/enrollAdmin.js
@@ -9,7 +9,7 @@ const { FileSystemWallet, X509WalletMixin } = require('fabric-network');
 const fs = require('fs');
 const path = require('path');
 
-const ccpPath = path.resolve(__dirname, '..', 'basic-network', 'connection.json');
+const ccpPath = path.resolve(__dirname, '..', '..', 'basic-network', 'connection.json');
 const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
 const ccp = JSON.parse(ccpJSON);
 
---
+++ chaincode/fabchat/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const FabChat = require('./lib/fabchat');
+
+module.exports.FabChat = FabChat;
+module.exports.contracts = [ FabChat ];
