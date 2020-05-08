ff7554153ffa2ea408692f9bcd373c3cd5c86fad
harpreetvirkk <harpreetvirk2018@gmail.com>
2020-05-09 01:11:39 +0530

    hello

---
+++ backup6/chaincode/fabchat/javascript/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const HyperVoter = require('./lib/hypervoter');
+
+module.exports.HyperVoter = HyperVoter;
+module.exports.contracts = [ HyperVoter ];
